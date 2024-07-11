a2a384b9883a096cb1908e9df3f6d1211cad9e4b
Abhishek Prasad <abhishekprasad1606@gmail.com>
2024-07-11 09:04:45 +0530

    Modified CSS of Login Pages

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ document.getElementById('login').addEventListener('click', function() {
     let button1 = document.createElement('button');
     button1.innerText = 'Login as a Teacher';
     button1.classList.add('button');
+    button1.onclick = function() {
+        window.location.href = 'professorLoginPage.html'; // Change this to your teacher signup page
+    };
     
     // Create the second additional button
     let button2 = document.createElement('button');
     button2.innerText = 'Login as a Student';
     button2.classList.add('button');
+    button2.onclick = function() {
+        window.location.href = 'studentLoginPage.html'; // Change this to your teacher signup page
+    };
     
     // Append the buttons to the container
     buttonContainer.appendChild(button1);
@@ -29,13 +35,22 @@ document.getElementById('signup').addEventListener('click', function() {
     let button1 = document.createElement('button');
     button1.innerText = 'Sign Up as a Teacher';
     button1.classList.add('button');
+    button1.onclick = function() {
+        window.location.href = 'notice.html'; // Change this to your teacher signup page
+    };
+    
     
     // Create the second additional button
     let button2 = document.createElement('button');
     button2.innerText = 'Sign Up as a Student';
     button2.classList.add('button');
+    button2.onclick = function() {
+        window.location.href = 'notice.html'; // Change this to your teacher signup page
+    };
     
     // Append the buttons to the container
     buttonContainer.appendChild(button1);
     buttonContainer.appendChild(button2);
+
+    
 });
